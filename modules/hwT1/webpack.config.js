dc082a1f0d62f19142e06bf0feaee6394a58587d
Valentin Panov <valentin.panov@me.com>
2021-02-07 21:52:41 +0300

    modules 1st done

---
+++ modules/hwT1/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const { Module } = require("webpack");
+
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+//const OptimizeCssAssetWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.bundle.js",
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+};
