794e9c86bd32fbca405d1590796281b0a1ca9a9e
Valentin Panov <vpanov@TEPEM.local>
2021-02-12 07:45:06 +0300

    unit-test init

--- unit-test/cleanFunctions/src/js/app.js
+++ unit-test/cleanFunctions/src/js/app.js
@@ -1,1 +1,14 @@
-// import sum from './basic';
+export default function healthStatus(obj) {
+  // {name: 'Маг', health: 90}
+  if (!obj.health) {
+    throw new Error('corrupted data request');
+  }
+  switch (obj.health) {
+    case obj.health > 50:
+      return 'healthy';
+    case obj.health < 15:
+      return 'critical';
+    default:
+      return 'wounded';
+  }
+}
