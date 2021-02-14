9e588c322d672a74e87becd5bc3d6f42987df78f
Valentin Panov <valentin.panov@me.com>
2021-02-14 18:52:21 +0300

    cont-proto 1st done

---
+++ prototypes-constructors/funcConstr/src/js/types.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    type: 'Bowman',
+    attack: 25,
+    defence: 25,
+  },
+  {
+    type: 'Swordsman',
+    attack: 40,
+    defence: 10,
+  },
+  {
+    type: 'Magician',
+    attack: 10,
+    defence: 40,
+  },
+  {
+    type: 'Daemon',
+    attack: 25,
+    defence: 25,
+  },
+  {
+    type: 'Undead',
+    attack: 40,
+    defence: 10,
+  },
+  {
+    type: 'Zombie',
+    attack: 10,
+    defence: 40,
+  },
+];
