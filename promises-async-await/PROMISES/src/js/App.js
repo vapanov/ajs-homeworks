7607b43c41701029009083182914eb579d6389db
Valentin Panov <valentin.panov@me.com>
2021-03-09 22:44:47 +0300

    fixed

--- promises-async-await/PROMISES/src/js/App.js
+++ promises-async-await/PROMISES/src/js/App.js
@@ -1,10 +1,10 @@
 import GameSavingLoader from './GameSavingLoader';
 import GameSaving from './GameSaving';
 
-const result = GameSavingLoader.load()
-  .then((saving) => new GameSaving(saving))
-  .catch((error) => {
-    throw new Error(error.message);
-  });
-
-export default result;
+export default new Promise((resolve) => {
+  GameSavingLoader.load()
+    .then((save) => resolve(new GameSaving(save)))
+    .catch((error) => {
+      throw new Error(error.message);
+    });
+});
--- promises-async-await/PROMISES/src/js/__tests__/App.test.js
+++ promises-async-await/PROMISES/src/js/__tests__/App.test.js
@@ -1,17 +1,13 @@
-import result from '../App';
+import saving from '../App';
 
-test('App should save the data', async () => {
-  const received = await result;
+test('App should save the data', () => {
   const expected = {
     id: 9,
     created: 1546300800,
     userInfo: { id: 1, name: 'Hitman', level: 10, points: 2000 },
   };
-  expect(received).toEqual(expected);
+  expect.assertions(1);
+  return saving.then((data) => {
+    expect(data).toEqual(expected);
+  });
 });
-
-// тест на заглушке
-// test('gameSavingLoader load() should throw an Err with empty data', async () => {
-//   const gameSavingLoader = new GameSavingLoader();
-//   expect(() => gameSavingLoader.load()).toThrowError();
-// });
